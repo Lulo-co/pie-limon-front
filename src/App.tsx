359df128cd15108df0aafe906abe8002b6326ec2
Erika Suárez Valencia <marvinesv@gmail.com>
2021-01-09 20:45:38 -0500

    Organize authorization flow

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,54 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { createUploadLink } from 'apollo-upload-client';
+import { onError } from '@apollo/client/link/error';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import GoogleLogin, { GoogleLoginResponse } from 'react-google-login';
 import KitchenTwoToneIcon from '@material-ui/icons/KitchenTwoTone';
 import React, { useState } from 'react';
 
 import { PAGES } from './AppConstants';
 import Catalog from './Catalog';
+import Oops from './Oops';
 import RecipeDetail from './RecipeDetail';
 
 import './App.css';
 
 function App() {
   const [token, setToken] = useState('');
+  const [isAllowed, setIsAllowed] = useState(true);
 
   const uploadLink = createUploadLink({
     uri: process.env.REACT_APP_GRAPHQL_URI,
     headers: {
       'keep-alive': 'true',
-      'Authorization': token,
+      Authorization: token,
     },
   });
+
+  const authLink = onError((errorResponse) => {
+    const a403 = errorResponse.graphQLErrors?.some(
+      (error) => error.extensions?.exception.status === 403
+    );
+    if (a403) setIsAllowed(false);
+  });
+
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: uploadLink,
+    link: authLink.concat(uploadLink),
   });
+
+  const loadRoute = (OkComponent: React.FC) => (): JSX.Element => {
+    if (!token || !isAllowed) {
+      return (
+        <Oops
+          loggedIn={token ? true : false}
+          allowed={isAllowed}
+          setToken={setToken}
+        />
+      );
+    }
+    return <OkComponent />;
+  };
 
   return (
     <div className="App">
@@ -44,25 +67,13 @@ function App() {
         </AppBar>
         <ApolloProvider client={client}>
           <Container>
-            {token ? (
-              <Switch>
-                <Route path={`${PAGES.RECIPE_DETAIL}:id`}>
-                  <RecipeDetail />
-                </Route>
-                <Route path={PAGES.ROOT}>
-                  <Catalog />
-                </Route>
-              </Switch>
-            ) : (
-                <GoogleLogin
-                  clientId={process.env.REACT_APP_GCLIENT_ID || ''}
-                  buttonText="Login"
-                  onSuccess={(response) => { setToken((response as GoogleLoginResponse).tokenId) }}
-                  onFailure={(p) => { console.error('failure', p) }}
-                  cookiePolicy={'single_host_origin'}
-                  isSignedIn
-                />
-              )}
+            <Switch>
+              <Route
+                path={`${PAGES.RECIPE_DETAIL}:id`}
+                render={loadRoute(RecipeDetail)}
+              />
+              <Route path={PAGES.ROOT} render={loadRoute(Catalog)} />
+            </Switch>
           </Container>
         </ApolloProvider>
       </Router>
